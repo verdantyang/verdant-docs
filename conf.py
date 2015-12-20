d6fb609de8fce396a514dddabd89539b1cfcf5e8
verdant <verdantyang@163.com>
2015-12-20 23:36:48 +0800

    modify conf

--- conf.py
+++ conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'Verdant Yang 学习笔记'
 copyright = u'2015, Verdant Yang'
-# author = u'Verdant Yang'
+author = u'Verdant Yang'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
